f66023371991e055a1194f805da3d669e0e5b034
acyeh-lab <133069589+acyeh-lab@users.noreply.github.com>
2025-09-26 07:54:12 -0700

    Add files via upload

--- scripts/download_assemblies/download_ref_dataset.py
+++ scripts/download_assemblies/download_ref_dataset.py
@@ -17,7 +17,7 @@ def download_ncbi_package(taxon_id, sci_name, outdir):
             "datasets", "download", "genome", "taxon", str(taxon_id),
             "--reference",
             "--annotated",
-            "--assembly-level", "complete",
+            ##"--assembly-level", "complete",
             "--include", "genome,protein,gff3",
             "--filename", zip_path
         ], check=True)
@@ -42,6 +42,26 @@ def download_ncbi_package(taxon_id, sci_name, outdir):
         subprocess.run(["unzip", "-o", zip_path, "-d", outdir], check=True)
     except subprocess.CalledProcessError:
         print(f"Failed to unzip for {sci_name} (TaxID: {taxon_id})")
+
+    # Generate data_summary.tsv
+    print("Generating data_summary.tsv...")
+    summary_path = os.path.join(outdir, "ncbi_dataset", "data", "data_summary.tsv")
+
+    try:
+        result = subprocess.run([
+            "dataformat", "tsv", "genome",
+            "--package", zip_path,
+            "--fields", "organism-name,organism-common-name,organism-infraspecific-strain,organism-tax-id,assminfo-name,accession,source_database,annotinfo-name,assminfo-level,assmstats-contig-n50,assmstats-total-sequence-len,assminfo-biosample-submission-date,annotinfo-featcount-gene-total,assminfo-bioproject,assminfo-biosample-accession"
+        ], capture_output=True, check=True, text=True)
+
+        with open(summary_path, "w") as f:
+            f.write(result.stdout)
+        print(f"Saved data_summary.tsv to {summary_path}")
+
+    except subprocess.CalledProcessError as e:
+        print("Failed to generate data_summary.tsv")
+        print(e.stderr)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Download NCBI genome data for taxon IDs listed in a CSV.")
